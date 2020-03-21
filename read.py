a047c42fa1f9f1a4c9847fa5d13a2f0665a21483
Sikuan Yan <yansikuan@gmail.com>
2020-03-21 12:24:51 +0100

    calc op

--- read.py
+++ read.py
@@ -4,9 +4,15 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		# if count % 1000 == 0:
+		# 	print(len(data))
 print(len(data))
 print(data[0])
 print('-----------------')
 print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('average length of comments is', sum_len/len(data))
